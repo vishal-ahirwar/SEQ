2a43e60f71d7955c3adeaef6ce97e7480a4bf72f
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2023-05-23 22:15:54 +0530

    project init

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class SEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    SEQAudioProcessorEditor (SEQAudioProcessor&);
+    ~SEQAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    SEQAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SEQAudioProcessorEditor)
+};
